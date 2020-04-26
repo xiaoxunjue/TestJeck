e4f8d9f43a97eaccaa66760cd7e562dbf4786fc1
wangsai <13081221277>
2020-04-26 14:41:41 +0800

    second

---
+++ logutils/src/main/java/com/revenant/logutils/LogUtils.java
@@ -0,0 +1,23 @@
+package com.revenant.logutils;
+
+import android.util.Log;
+
+/**
+ * @ProjectName: TestJeck
+ * @Package: com.revenant.logutils
+ * @ClassName: LogUtils
+ * @Description: java类作用描述
+ * @Author: Revenant
+ * @CreateDate: 2020/4/26 14:37
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2020/4/26 14:37
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class LogUtils {
+
+    public   static  void logTest()  {
+        Log.d("TestLog","TestLog");
+    }
+
+}
